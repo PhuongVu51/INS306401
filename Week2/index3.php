b0cd85bb6a60c73e288c874f592dde968b8a9a95
PhuongVu51 <23070575@vnu.edu.vn>
2025-09-23 12:26:50 +0700

    day2

---
+++ Week2/index3.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>This is my first PHP</h1>
+    <p>
+        This is x value:
+        <?php
+            $x = 5;
+            echo "The value of x is: " . $x;
+        ?>
+    </p>
+</body>
+</html>
